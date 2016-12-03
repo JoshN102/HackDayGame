258ff16d7428b31165f93c5b891670e9afb5dbcb
Alfred Godley <Alfred@Alfreds-MacBook-Pro.local>
2016-12-03 18:15:44 +0000

    added support for triangles and circles

--- TEST.py
+++ TEST.py
@@ -7,6 +7,7 @@ square = square.square(screen)
 clock = pygame.time.Clock()
 isRunning = True
 
+shape = 's'
 while isRunning:
     
     for event in pygame.event.get():
@@ -14,12 +15,18 @@ while isRunning:
         mousePressed = pygame.mouse.get_pressed()
         if event.type == pygame.QUIT:
             isRunning = False
-        if mousePressed[0]:
-            (x,y) = pygame.mouse.get_pos()
-            square.draw(x,y)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
+            elif event.key == pygame.K_s:
+                shape = 's'
+            elif event.key == pygame.K_c:
+                shape = 'c'
+            elif event.key == pygame.K_t:
+                shape = 't'
+        if mousePressed[0]:
+            (x, y) = pygame.mouse.get_pos()
+            square.draw(x, y, shape)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
