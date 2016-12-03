1a6f766855ea980435cdd1bb84b8a10890f6c7ec
Josh New <Josh New>
2016-12-03 15:31:05 +0000

    Changes to square

--- square.py
+++ square.py
@@ -6,9 +6,10 @@ class square:
         self.screen = screen
         
     def draw(self,x,y):
-        rect = pygame.rect = (x,y,100,100)
+        rect = pygame.rect = (x-50,y-50,100,100)
         pygame.draw.rect(self.screen,(0,128,255),rect)
         pygame.display.flip()
             
     def clear(self):
         self.screen.fill((0,0,0))
+        pygame.display.flip()
